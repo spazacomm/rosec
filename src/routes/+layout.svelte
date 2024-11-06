2d8768e192b5f42ff805f18ffdcdc0c2c4fba0cb
spaza <spaza@spaza.spaza.net>
2024-11-06 18:26:33 +0300

    content update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,29 @@
-<script>
-	import '../app.css';
-	let { children } = $props();
+<!-- src/routes/+layout.svelte -->
+<script lang="ts">
+  import '../styles.css'
+  import { invalidate } from '$app/navigation'
+  import { onMount } from 'svelte'
+
+  export let data
+
+  let { supabase, session } = data
+  $: ({ supabase, session } = data)
+
+  onMount(() => {
+    const { data } = supabase.auth.onAuthStateChange((event, newSession) => {
+      if (newSession?.expires_at !== session?.expires_at) {
+        invalidate('supabase:auth')
+      }
+    })
+
+    return () => data.subscription.unsubscribe()
+  })
 </script>
 
-{@render children()}
+<svelte:head>
+  <title>User Management</title>
+</svelte:head>
+
+<div class="container" style="padding: 50px 0 100px 0">
+  <slot />
+</div>
